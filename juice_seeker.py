fa3cc22991c72b12a062d9667254af85e9bc239e
George Chen <gechen@zoox.com>
2022-12-20 12:56:02 -0800

    No more time-based waiting

--- juice_seeker.py
+++ juice_seeker.py
@@ -11,33 +11,7 @@ import requests
 import numpy as np
 import scipy.interpolate as si
 
-# Curve base:
-points = [[0, 0], [0, 2], [2, 3], [4, 0], [6, 3], [8, 2], [8, 0]];
-points = np.array(points)
-
-x = points[:,0]
-y = points[:,1]
-
-
-t = range(len(points))
-ipl_t = np.linspace(0.0, len(points) - 1, 100)
-
-x_tup = si.splrep(t, x, k=3)
-y_tup = si.splrep(t, y, k=3)
-
-x_list = list(x_tup)
-xl = x.tolist()
-x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]
-
-y_list = list(y_tup)
-yl = y.tolist()
-y_list[1] = yl + [0.0, 0.0, 0.0, 0.0]
-
-x_i = si.splev(ipl_t, x_list) # x interpolate values
-y_i = si.splev(ipl_t, y_list) # y interpolate values
-
-
-REFRESH_TIME = 0.5 # sec
+REFRESH_TIME = 0.5  # sec
 
 print("Getting browser...")
 # options = webdriver.ChromeOptions() #options. ...
@@ -45,26 +19,16 @@ driver = webdriver.Chrome()
 
 print("Opening website...")
 # Opening the website
-driver.get("https://sky.shellrecharge.com/evowner/portal/locate-charger")
+driver.get("https://sky.shellrecharge.com/evowner/account/sign-in")
 
-# finding the button using ID
-button = driver.find_element(value="driver-login")
-
-# clicking on the button
-button.click()
-
-while not driver.find_elements(value="mat-input-2"):
+while not driver.find_elements(value="mat-input-1"):
   continue
 
-email_txtbx = driver.find_element(value="mat-input-1")
-email_txtbx.send_keys("***********@gmail.com") #
-pw_txtbx = driver.find_element(value="mat-input-2")
-pw_txtbx.send_keys("***************") #
-time.sleep(5)
-# TODO: not working yet
-# for mouse_x, mouse_y in zip(x_i, y_i):
-#     ActionChains(driver).move_by_offset(mouse_x, mouse_y)
-#     ActionChains(driver).perform()
+email_txtbx = driver.find_element(value="mat-input-0")
+email_txtbx.send_keys("***********@gmail.com")
+pw_txtbx = driver.find_element(value="mat-input-1")
+pw_txtbx.send_keys("***************")
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 # find iframe
 captcha_iframe = WebDriverWait(driver, 2).until(
@@ -88,9 +52,98 @@ captcha_box = WebDriverWait(driver, 2).until(
 
 driver.execute_script("arguments[0].click()", captcha_box)
 print('I am not a robot I swear!')
-time.sleep(2)
+
+audio_button_found = False
+while not driver.find_elements(by=By.CLASS_NAME, value="recaptcha-checkbox-checkmark"):
+  if audio_button_found:
+    pass # TODO
+  continue
+
+print("Tricked you there lol")
+
+# finding the button using ID
+login = driver.find_element(by=By.CLASS_NAME, value="green_btn_lg.customize-primary-bg.mat-button")
+
+# clicking on the button
+login.click()
+
+print("Logging in...")
+while driver.find_elements(value="mat-input-1"):
+  continue
+print("Login successful, getting stations...")
+
+def get_availabilities(driver):
+  output_dict = dict()
+  html = BeautifulSoup(driver.page_source, features="lxml")
+  for card in html.body.find_all('mat-card', attrs={"class": "notify_list"}):
+    station_id = int(
+        card.find('div', attrs={"class": "charger-detail-head"}).span.text.split(':')[-1])
+    output_dict.setdefault(station_id, dict())
+    chargers = card.find('div', attrs={"class": "charger-detail-body"}
+                         ).find_all('div', attrs={"class": "charger_info"})
+    for charger in chargers:
+      charger_num = int(charger.find('div', attrs={"class": "number_charge"}).span.text)
+      charger_avail = charger.find(
+          'div', attrs={"class": "available_info"}).span.text.lstrip('(').rstrip(')')
+      output_dict[station_id][charger_num] = charger_avail
+  return output_dict
+
+
+driver.get('https://sky.shellrecharge.com/evowner/portal/manage-account/favorites')
+while not any([el.text == "521407_4100 Bayside" for el in driver.find_elements(by=By.TAG_NAME, value="a")]):
+  continue
+back_spots = [el for el in driver.find_elements(
+    by=By.TAG_NAME, value="a") if el.text == "521407_4100 Bayside"][0]
+back_spots.click()
+while not driver.find_elements(by=By.CLASS_NAME, value="charger-detail-head"):
+  continue
+
+back_availabilities = get_availabilities(driver)
+
+driver.get('https://sky.shellrecharge.com/evowner/portal/manage-account/favorites')
+while not any([el.text == "521412_4000 Bayside" for el in driver.find_elements(by=By.TAG_NAME, value="a")]):
+  continue
+back_spots = [el for el in driver.find_elements(
+    by=By.TAG_NAME, value="a") if el.text == "521412_4000 Bayside"][0]
+back_spots.click()
+while not driver.find_elements(by=By.CLASS_NAME, value="charger-detail-head"):
+  continue
+
+front_availabilities = get_availabilities(driver)
+
+print(back_availabilities)
+print(front_availabilities)
+
+driver.close()
+
 
 # ============================================================================
+# # Curve base:
+# points = [[0, 0], [0, 2], [2, 3], [4, 0], [6, 3], [8, 2], [8, 0]]
+# points = np.array(points)
+
+# x = points[:,0]
+# y = points[:,1]
+
+
+# t = range(len(points))
+# ipl_t = np.linspace(0.0, len(points) - 1, 100)
+
+# x_tup = si.splrep(t, x, k=3)
+# y_tup = si.splrep(t, y, k=3)
+
+# x_list = list(x_tup)
+# xl = x.tolist()
+# x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]
+
+# y_list = list(y_tup)
+# yl = y.tolist()
+# y_list[1] = yl + [0.0, 0.0, 0.0, 0.0]
+
+# x_i = si.splev(ipl_t, x_list) # x interpolate values
+# y_i = si.splev(ipl_t, y_list) # y interpolate values
+
+
 # audioBtnFound = False
 # audioBtnIndex = -1
 # filename = '1.mp3'
@@ -142,7 +195,7 @@ time.sleep(2)
 #     break
 # except Exception as e:
 #     pass
-      
+
 # if audioBtnFound:
 #     try:
 #         while True:
@@ -168,51 +221,3 @@ time.sleep(2)
 #         print('Button not found. This should not happen.')
 
 # ============================================================================
-time.sleep(10)
-
-# finding the button using ID
-login = driver.find_element(by=By.CLASS_NAME, value="green_btn_lg.customize-primary-bg.mat-button")
-
-# clicking on the button
-login.click()
-print("Logging in...")
-while driver.find_elements(value="mat-input-2"):
-  continue
-
-def get_availabilities(driver):
-  output_dict = dict()
-  html = BeautifulSoup(driver.page_source, features="lxml")
-  for card in html.body.find_all('mat-card', attrs={"class": "notify_list"}):
-    station_id = int(card.find('div', attrs={"class": "charger-detail-head"}).span.text.split(':')[-1])
-    output_dict.setdefault(station_id, dict())
-    chargers = card.find('div', attrs={"class": "charger-detail-body"}).find_all('div', attrs={"class": "charger_info"})
-    for charger in chargers:
-      charger_num = int(charger.find('div', attrs={"class": "number_charge"}).span.text)
-      charger_avail = charger.find('div', attrs={"class": "available_info"}).span.text.lstrip('(').rstrip(')')
-      output_dict[station_id][charger_num] = charger_avail
-  return output_dict
-
-driver.get('https://sky.shellrecharge.com/evowner/portal/manage-account/favorites')
-while not any([el.text == "521407_4100 Bayside" for el in driver.find_elements(by=By.TAG_NAME, value="a")]):
-  continue
-back_spots = [el for el in driver.find_elements(by=By.TAG_NAME, value="a") if el.text == "521407_4100 Bayside"][0]
-back_spots.click()
-while not driver.find_elements(by=By.CLASS_NAME, value="charger-detail-head"):
-  continue
-
-back_availabilities = get_availabilities(driver)
-  
-driver.get('https://sky.shellrecharge.com/evowner/portal/manage-account/favorites')
-while not any([el.text == "521412_4000 Bayside" for el in driver.find_elements(by=By.TAG_NAME, value="a")]):
-  continue
-back_spots = [el for el in driver.find_elements(by=By.TAG_NAME, value="a") if el.text == "521412_4000 Bayside"][0]
-back_spots.click()
-while not driver.find_elements(by=By.CLASS_NAME, value="charger-detail-head"):
-  continue
-
-front_availabilities = get_availabilities(driver)
-
-print(back_availabilities)
-print(front_availabilities)
-
-driver.close()
