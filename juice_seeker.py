da1ead93b69cbd79e62e42aaaa009a31896cf6f2
George Chen <gechen@zoox.com>
2023-08-28 16:47:53 -0700

    No longer broken, can bypass website with manual login

--- juice_seeker.py
+++ juice_seeker.py
@@ -2,6 +2,7 @@ import pprint
 import logging
 import json
 import os
+import time
 from utils import *
 from selenium import webdriver
 from selenium.webdriver import ActionChains
@@ -15,7 +16,9 @@ from dotenv import load_dotenv
 load_dotenv()
 pp = pprint.PrettyPrinter(indent=2)
 
+MANUAL_LOGIN = True
 DEBUG_MODE = False
+CLICK_DELAY = 5
 
 # These stations and ports don't work
 EXCLUDE_FRONT = [(52447, 2), (52957, 1), (52958, 1)]
@@ -39,10 +42,32 @@ else:
 logger = logging.getLogger()
 
 def setup():
-  logger.superinfo("Getting browser...")
-  driver = webdriver.Chrome()
+  logger.superinfo("Getting browser...") 
+  # Create Chromeoptions instance 
+  options = webdriver.ChromeOptions() 
+  
+  options.add_argument("--incognito")
+  
+  options.add_argument("--ignore-certificate-errors")
+  
+  options.add_argument("--remote-allow-origins=*")
+  
+  # Adding argument to disable the AutomationControlled flag 
+  options.add_argument("--disable-blink-features=AutomationControlled") 
+  
+  # Exclude the collection of enable-automation switches 
+  options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+  
+  # Turn-off userAutomationExtension 
+  options.add_experimental_option("useAutomationExtension", False) 
+  
+  # Setting the driver path and requesting a page 
+  driver = webdriver.Chrome(options=options) 
+  
+  # Changing the property of the navigator value for webdriver to undefined 
+  driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
 
-  # Limitation: during login, must have browser window open on the same screen (not necessarily selected)
+  # Limitation: during login, must have browser window open on screen (not necessarily selected)
   # TODO: enable headless mode:
   # options = webdriver.ChromeOptions()
   # options.headless = True
@@ -59,73 +84,75 @@ def setup():
   email_txtbx.send_keys(os.environ['GREENSHELL_USERNAME'])
   pw_txtbx = driver.find_element(value="mat-input-1")
   pw_txtbx.send_keys(os.environ['GREENSHELL_PASSWORD'])
-  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+  
+  if not MANUAL_LOGIN:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-  # find iframe
-  captcha_iframe = WebDriverWait(driver, 2).until(
-      ec.presence_of_element_located(
-          (
-              By.TAG_NAME, 'iframe'
-          )
-      )
-  )
+    # find iframe
+    captcha_iframe = WebDriverWait(driver, 2).until(
+        ec.presence_of_element_located(
+            (
+                By.TAG_NAME, 'iframe'
+            )
+        )
+    )
 
-  ActionChains(driver).move_to_element(captcha_iframe).click().perform()
+    ActionChains(driver).move_to_element(captcha_iframe).click().perform()
 
-  captcha_box = WebDriverWait(driver, 2).until(
-      ec.presence_of_element_located(
-          (
-              By.ID, 'g-recaptcha-response'
-          )
-      )
-  )
+    captcha_box = WebDriverWait(driver, 2).until(
+        ec.presence_of_element_located(
+            (
+                By.ID, 'g-recaptcha-response'
+            )
+        )
+    )
 
-  driver.execute_script("arguments[0].click()", captcha_box)
-  logger.superinfo('I am not a robot I swear!')
+    driver.execute_script("arguments[0].click()", captcha_box)
+    logger.superinfo('I am not a robot I swear!')
 
-  # Do the (likely) audio challenge for captcha
-  captcha_ok = False
-  audio_sent = False
+    # Do the audio challenge for captcha
+    captcha_ok = False
+    audio_sent = False
 
-  while not captcha_ok:
-    driver.switch_to.frame(captcha_iframe)
-    holder = driver.find_element(by=By.CLASS_NAME, value='rc-anchor-checkbox-holder')
-    span = holder.find_element(by=By.TAG_NAME, value='span')
+    while not captcha_ok:
+      driver.switch_to.frame(captcha_iframe)
+      holder = driver.find_element(by=By.CLASS_NAME, value='rc-anchor-checkbox-holder')
+      span = holder.find_element(by=By.TAG_NAME, value='span')
 
-    if "recaptcha-checkbox-checked" in span.get_attribute("class"):
-      captcha_ok = True
-      logger.superinfo("Got u good lmao")
-    elif not audio_sent:
+      if "recaptcha-checkbox-checked" in span.get_attribute("class"):
+        captcha_ok = True
+        logger.superinfo("Got u good lmao")
+      elif not audio_sent:
+        driver.switch_to.default_content()
+        iframe = driver.find_element(
+            by=By.XPATH, value="//iframe[@title='recaptcha challenge expires in two minutes']")
+        driver.switch_to.frame(iframe)
+        audio_button_list = driver.find_elements(by=By.ID, value='recaptcha-audio-button')
+        if audio_button_list:
+          logger.superinfo("Audio button found")
+          driver.execute_script("arguments[0].click()", audio_button_list[0])
+          while not driver.find_elements(by=By.CLASS_NAME, value="rc-audiochallenge-tdownload-link"):
+            continue
+          audio_link_div = driver.find_element(
+              by=By.XPATH, value="//a[@class='rc-audiochallenge-tdownload-link']")
+          audio_link = audio_link_div.get_attribute('href')
+          audio = get_challenge_audio(audio_link)
+          audio_output = speech_to_text(audio)
+          driver.find_element(by=By.ID, value='audio-response').send_keys(audio_output)
+          driver.find_element(by=By.ID, value='recaptcha-verify-button').click()
+          audio_sent = True
       driver.switch_to.default_content()
-      iframe = driver.find_element(
-          by=By.XPATH, value="//iframe[@title='recaptcha challenge expires in two minutes']")
-      driver.switch_to.frame(iframe)
-      audio_button_list = driver.find_elements(by=By.ID, value='recaptcha-audio-button')
-      if audio_button_list:
-        logger.superinfo("Audio button found")
-        driver.execute_script("arguments[0].click()", audio_button_list[0])
-        while not driver.find_elements(by=By.CLASS_NAME, value="rc-audiochallenge-tdownload-link"):
-          continue
-        audio_link_div = driver.find_element(
-            by=By.XPATH, value="//a[@class='rc-audiochallenge-tdownload-link']")
-        audio_link = audio_link_div.get_attribute('href')
-        audio = get_challenge_audio(audio_link)
-        audio_output = speech_to_text(audio)
-        driver.find_element(by=By.ID, value='audio-response').send_keys(audio_output)
-        driver.find_element(by=By.ID, value='recaptcha-verify-button').click()
-        audio_sent = True
-    driver.switch_to.default_content()
 
-  can_login = False
+    can_login = False
 
-  while not can_login:
-    # find the login button
-    login = driver.find_element(
-      by=By.CLASS_NAME, value="green_btn_lg.customize-primary-bg.mat-button")
-    can_login = (login.get_attribute("disabled") != 'true')
+    while not can_login:
+      # find the login button
+      login = driver.find_element(
+        by=By.CLASS_NAME, value="green_btn_lg.customize-primary-bg.mat-button")
+      can_login = (login.get_attribute("disabled") != 'true')
 
-  # clicking on the button
-  login.click()
+    # clicking on the button
+    login.click()
 
   logger.superinfo("Logging in...")
   while driver.find_elements(value="mat-input-1"):
@@ -160,28 +187,30 @@ def get_front_and_back(driver):
       continue
     back_spots = [el for el in driver.find_elements(
         by=By.TAG_NAME, value="a") if el.text == "521407_4100 Bayside"][0]
+    time.sleep(CLICK_DELAY)
     back_spots.click()
     while not driver.find_elements(by=By.CLASS_NAME, value="charger-detail-head"):
       continue
 
     back_availabilities = read_station(driver)
-    logger.info(f"Looked at the back")
+    logger.info("Looked at the back")
     logger.info(json.dumps(back_availabilities, indent=2))
-
-    driver.get('https://sky.shellrecharge.com/evowner/portal/manage-account/favorites')
+    time.sleep(CLICK_DELAY)
+    driver.back()
     while not any([el.text == "521412_4000 Bayside" for el in driver.find_elements(by=By.TAG_NAME, value="a")]):
       continue
-    back_spots = [el for el in driver.find_elements(
+    logger.info("Looking at the front")
+    front_spots = [el for el in driver.find_elements(
         by=By.TAG_NAME, value="a") if el.text == "521412_4000 Bayside"][0]
-    back_spots.click()
+    time.sleep(CLICK_DELAY)
+    front_spots.click()
     while not driver.find_elements(by=By.CLASS_NAME, value="charger-detail-head"):
       continue
+    front_availabilities = read_station(driver)
+    logger.info("Looked at the front")
+    logger.info(json.dumps(front_availabilities, indent=2))
   except:
     return None, None
-
-  front_availabilities = read_station(driver)
-  logger.info(f"Looked at the front")
-  logger.info(json.dumps(front_availabilities, indent=2))
 
   return front_availabilities, back_availabilities
 
